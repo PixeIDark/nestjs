4d5f3437adec08e30ef33a54a045d33ae1b08878
Darkthos <kiki7951@naver.com>
2024-09-29 02:24:31 +0900

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirstController } from './first/first.controller';
+import { MountModule } from './mount/mount.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [MountModule],
+  controllers: [AppController, FirstController],
   providers: [AppService],
 })
 export class AppModule {}
